75adabd33c2a3c059b7462e597e9685928a63d6a
Muhammad Nakib Uddin <fliptechofficial@gmail.com>
2026-06-01 10:00:01 +0000

    Add path and URL utilities for improved file handling in app.js

--- src/app.js
+++ src/app.js
@@ -1,12 +1,17 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url"; 
 import middleWares from "./middleware.js";
-import testRouter from "./Routs/test.js"
+import testRouter from "./Routs/test.js";
 import { connectDB } from "./db.js";
 dotenv.config();
 const port = process.env.PORT || 4300;
 const app = express();
 const dev = !process.env.VERCEL;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 middleWares(app);
 connectDB().catch(console.dir);
@@ -19,7 +24,7 @@ app.get('/', (req, res) => {
 
 app.use("/test", testRouter);
 
-if(dev){
+if (dev) {
   app.listen(port, () => {
     console.log(`server started on port ${port}.`);
   });
