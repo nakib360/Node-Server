10728d336e0826b4c0c6a6eec5eef1c6e3178c92
Muhammad Nakib Uddin <fliptechofficial@gmail.com>
2026-06-01 09:44:00 +0000

    Refactor server startup log and update Vercel configuration with routing

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 import express from "express";
 import dotenv from "dotenv";
-import terminalLink from "terminal-link";
 import middleWares from "./middleware.js";
 import testRouter from "./Routs/test.js"
 import { connectDB } from "./db.js";
@@ -16,9 +15,7 @@ app.use("/test", testRouter);
 
 if(dev){
   app.listen(port, () => {
-    const url = `http://localhost:${port}`;
-    const clickable = terminalLink(url, url);
-    console.log(`server started in dev mood.\nctrl + click ${clickable}`);
+    console.log(`server started on port ${port}.`);
   });
 }
 
